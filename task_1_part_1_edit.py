6dca682671c7247403e613a85061e49c2fe0708e
Artem <artemdodosian@gmail.com>
2022-08-23 20:14:51 +0300

    two scenarios have been added

---
+++ task_1_part_1_edit.py
@@ -0,0 +1,31 @@
+#level 1
+'''value = ((7*5)/8+5)/3**5
+print(value)'''
+
+#level 2
+'''v = int(input('Speed: '))
+t = int(input('Time: '))
+if v*t > 109:
+	print(v*t%109)
+else:
+	print(v*t)'''
+
+
+#level 3
+'''num1 = float(input('Enter the first number: '))
+num2 = float(input('Enter the second number: '))
+if num1 > num2:
+	print(num1)
+elif num1 == num2:
+	print(num1)
+else:
+	print(num2)
+
+      #or
+
+max1 = (num1 > num2)*num1 or (num1 <= num2)*num2
+#print(max1) 
+
+	  #or
+
+print(max(num1, num2))'''     
---
+++ task_1_part_2.py
@@ -0,0 +1,31 @@
+#level 1
+'''num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+num3 = int(input('Enter the third number: '))
+
+if num1 == num2 == num3:
+	print('3')
+elif num1 == num2 or num1 == num3 or num2 == num3:
+	print('2')
+else:
+	print('0')'''
+
+
+#level 2
+'''x1 = int(input('Enter the first number of square: '))
+y1 = int(input('Enter the second number of square: '))
+
+x2 = int(input('Enter the first number of square: '))
+y2 = int(input('Enter the second number of square: '))
+
+if x1 == x2 or y1 == y2:
+	print('YES')
+else:
+	print('NO')'''
+
+
+#level 3
+'''password = input('enter the password: ')
+while len(password)<8:
+ 	password = input('enter the password: ')'''
+ 
